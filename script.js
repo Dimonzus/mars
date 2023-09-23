b6cf00cbfe9c74e75dfc3153a67b05755a35d425
Dima <jimbin23@gmail.com>
2023-09-23 08:10:16 +0300

    Created function makePhone

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-num = [1, 2, 3]
+num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+
+const makePhone = () => {
+  return `Phone number: (${num.slice(0, 3).join("")}) ${num
+    .slice(3, 6)
+    .join("")}-${num.slice(6, 8).join("")}-${num.slice(8, 10).join("")} `
+}
+
+console.log(makePhone())
