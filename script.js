338ae53c2e7356e942514719b45b5e039646742c
Dima <jimbin23@gmail.com>
2023-09-23 08:14:03 +0300

    function makePhone without space

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
 
 const makePhone = (arr) => {
-  return `Phone number: (${arr.slice(0, 3).join("")}) ${arr
+  return `Phone number: (${arr.slice(0, 3).join("")})${arr
     .slice(3, 6)
     .join("")}-${arr.slice(6, 8).join("")}-${arr.slice(8, 10).join("")} `
 }
