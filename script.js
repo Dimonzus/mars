754e58ec482c61fe8d3be3816f085554b8813543
Dima <jimbin23@gmail.com>
2023-09-23 08:20:02 +0300

    Added condition

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+num = [1, 2, 3, 4, 5, 6, 7, 8, 0, 9]
 
 const makePhone = (arr) => {
-  return `Phone number: (${arr.slice(0, 3).join("")})${arr
-    .slice(3, 6)
-    .join("")}-${arr.slice(6, 8).join("")}-${arr.slice(8, 10).join("")} `
+  if (arr.length == 10) {
+    return `Phone number: (${arr.slice(0, 3).join("")})${arr
+      .slice(3, 6)
+      .join("")}-${arr.slice(6, 8).join("")}-${arr.slice(8, 10).join("")} `
+  } else {
+    return "Check input length"
+  }
 }
 
 console.log(makePhone(num))
